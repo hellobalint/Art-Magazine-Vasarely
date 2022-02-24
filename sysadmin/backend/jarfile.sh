74704643aa72c50373584d20468b4a95f19ee76b
stpn77 <92166098+stpn77@users.noreply.github.com>
2022-02-24 08:32:30 +0100

    config files updated

--- sysadmin/backend/jarfile.sh
+++ sysadmin/backend/jarfile.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# old test versiohn
 nohup java -jar /home/ubuntu/jar/fileshare-0.0.1-SNAPSHOT.jar &>/dev/null &
+
+# 2022.02.24 new version
+source /home/ubuntu/jar/db_init.sh // needs to be executable! chmod +x db_init.sh 
+nohup java -jar /home/ubuntu/jar/fileshare-0.0.1-SNAPSHOT.jar &>/dev/null &
+# manual start: nohup java -jar /home/ubuntu/jar/artworkConsole.jar &>/dev/null &
--- sysadmin/backend/script.md
+++ sysadmin/backend/script.md
@@ -1,7 +1,6 @@
 sudo systemctl status tomcat.service // check if tomcat is running. if yes, stop it
 sudo systemctl disable tomcat.service
 
-
 chmod +x /home/ubuntu/jarfile.sh // make script executable
 
 crontab -e // open crontab and add line bellow
